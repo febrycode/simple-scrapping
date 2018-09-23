55552956efd933a60f6ea5d15ccfeb2c5f67fd22
Febryan Setiawan <setiawanfebryan@gmail.com>
2018-09-23 15:23:05 +0700

    add model Job

---
+++ model/Job.js
@@ -0,0 +1,17 @@
+const mongoose = require('../config/db')
+
+const nameSchema = new mongoose.Schema(
+  {
+    jobName: String,
+    companyName: String,
+    companyLogo: String,
+    jobLocation: String,
+    jobFunction: String,
+    jobIndustry: String
+  }, {
+    timestamps: true
+  })
+
+const Job = mongoose.model('Job', nameSchema)
+
+module.exports = Job
