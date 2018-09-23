10c7a346ab44ec14945ddf5655c86b010c2e7fbe
Febryan Setiawan <setiawanfebryan@gmail.com>
2018-09-23 17:16:06 +0700

    add routes file

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const jobService = require('../services/jobs')
+
+function init (server) {
+  server.get('*', function (req, res, next) {
+    console.log('Request was made to: ' + req.originalUrl)
+    return next()
+  })
+
+  server.get('/scrapes', jobService.scrapeJob)
+  server.get('/jobs', jobService.getAll)
+  server.post('/jobs', jobService.createJob)
+  server.put('/jobs/:id', jobService.updateJob)
+  server.delete('/jobs/:id', jobService.deleteJob)
+}
+
+module.exports = {
+  init
+}
