633649bf11d3a59690b88646a1844186417607de
Febryan Setiawan <setiawanfebryan@gmail.com>
2018-09-24 10:01:30 +0700

    add crud integration test

--- test/integration/jobs.integration.test.js
+++ test/integration/jobs.integration.test.js
@@ -21,10 +21,120 @@ describe('Job', () => {
         .get('/jobs')
         .end((err, res) => {
           should.not.exist(err)
+          res.body.should.be.a('object')
           res.status.should.equal(200)
           res.body.error.should.equal(false)
+          res.body.jobs.length.should.be.equal(0)
           done()
       })
     })
   })
+
+  describe('GET /jobs/:id', () => {
+    it('it should GET a job by the given id', (done) => {
+      const job = new Job({
+        jobName: 'Programmer',
+        companyName: 'Google',
+        companyLogo: 'http://facebook.com',
+        jobLocation: 'California',
+        jobFunction: 'testing',
+        jobIndustry: 'IT'
+      })
+
+      job.save((err, job) => {
+        chai.request(server)
+          .get('/jobs/' + job.id)
+          .end((err, res) => {
+            should.not.exist(err)
+            res.status.should.equal(200)
+            res.body.error.should.equal(false)
+            res.body.job.should.have.property('jobName').eql(job.jobName)
+            res.body.job.should.have.property('_id').eql(job.id)
+            done()
+          })
+      })
+    })
+  })
+
+  describe('POST /jobs', () => {
+    it('it should POST', (done) => {
+      const job = {
+        jobName: 'Programmer',
+        companyName: 'Google',
+        companyLogo: 'http://facebook.com',
+        jobLocation: 'California',
+        jobFunction: 'testing',
+        jobIndustry: 'IT'
+      }
+
+      chai.request(server)
+        .post('/jobs')
+        .send(job)
+        .end((err, res) => {
+          should.not.exist(err)
+          res.status.should.equal(200)
+          res.body.error.should.equal(false)
+          res.body.should.have.property('message').eql('Data has been created successully')
+          done()
+        })
+    })
+  })
+
+  describe('GET /scrapes', () => {
+    // PENDING
+    it('it should scrapes from websites and save to database')
+  })
+
+  describe('PUT /jobs/:id', () => {
+    it('it should update a job by the given id', (done) => {
+      const job = new Job({
+        jobName: 'Programmer',
+        companyName: 'Google',
+        companyLogo: 'http://facebook.com',
+        jobLocation: 'California',
+        jobFunction: 'testing',
+        jobIndustry: 'IT'
+      })
+
+      job.save((err, job) => {
+        chai.request(server)
+          .put('/jobs/' + job.id)
+          .send({ jobName: 'Test Job' })
+          .end((err, res) => {
+            should.not.exist(err)
+            res.status.should.equal(200)
+            res.body.error.should.equal(false)
+            res.body.should.have.property('message').eql('Data has been updated successfuly.')
+            done()
+          })
+      })
+    })
+  })
+
+  describe('DELETE /jobs/:id', () => {
+    it('it should delete a job by the given id', (done) => {
+      const job = new Job({
+        jobName: 'Programmer',
+        companyName: 'Google',
+        companyLogo: 'http://facebook.com',
+        jobLocation: 'California',
+        jobFunction: 'testing',
+        jobIndustry: 'IT'
+      })
+
+      job.save((err, job) => {
+        chai.request(server)
+          .delete('/jobs/' + job.id)
+          .send({ jobName: 'Test Job' })
+          .end((err, res) => {
+            should.not.exist(err)
+            res.status.should.equal(200)
+            res.body.error.should.equal(false)
+            res.body.should.have.property('message').eql('Data has been deleted successfuly.')
+            done()
+          })
+      })
+    })
+  })
+
 })
